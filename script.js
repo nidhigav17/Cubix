a28edd1ef2b3770f89c5d6ddbfda8b890ed0d684
nidhigavshindhe <nidhidodegavshindhe@gmail.com>
2024-09-24 17:57:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeScroll(){
+    let navbar=document.getElementById("nav-scroll");
+    let scrollValue=window.scrollY;
+    //console.log(scrollY);
+    if(scrollValue < 500){
+        navbar.classList.remove("bgColor");
+    }else{
+        navbar.classList.add("bgColor");
+    }
+    
+}
+window.addEventListener("scroll",changeScroll);
